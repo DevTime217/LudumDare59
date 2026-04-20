09729e9aea8ef4f332126612f3c0477066302ed2
DevTime217 <headshotgamingmaster@gmail.com>
2026-04-20 18:15:41 -0400

    Button, Exit, and Door Created

--- scenes/objects/player.gd
+++ scenes/objects/player.gd
@@ -1,16 +1,20 @@
 extends CharacterBody2D
 
-const tile_size: Vector2 = Vector2(64, 64)
+const tile_size: Vector2 = Vector2(128, 128)
 
 func _physics_process(delta):
-	if Input.is_action_just_pressed("move_up") and !$Up.is_colliding():
-		_move(Vector2(0, -1))
-	elif Input.is_action_just_pressed("move_down") and !$Down.is_colliding():
-		_move(Vector2(0, 1))
-	elif Input.is_action_just_pressed("move_left") and !$Left.is_colliding():
-		_move(Vector2(-1, 0))
-	elif Input.is_action_just_pressed("move_right") and !$Right.is_colliding():
-		_move(Vector2(1, 0))
+	if Input.is_action_just_pressed("move_up"):
+		if !$Up.is_colliding() or ($Up.get_collider() != null and ($Up.get_collider().is_in_group("Button") or $Up.get_collider().is_in_group("Exit"))):
+			_move(Vector2(0, -1))
+	elif Input.is_action_just_pressed("move_down"):
+		if !$Down.is_colliding() or ($Down.get_collider() != null and ($Down.get_collider().is_in_group("Button") or $Down.get_collider().is_in_group("Exit"))):
+			_move(Vector2(0, 1))
+	elif Input.is_action_just_pressed("move_left"):
+		if !$Left.is_colliding() or ($Left.get_collider() != null and ($Left.get_collider().is_in_group("Button") or $Left.get_collider().is_in_group("Exit"))):
+			_move(Vector2(-1, 0))
+	elif Input.is_action_just_pressed("move_right"):
+		if !$Right.is_colliding() or ($Right.get_collider() != null and ($Right.get_collider().is_in_group("Button") or $Right.get_collider().is_in_group("Exit"))):
+			_move(Vector2(1, 0))
 
 func _move(dir: Vector2):
 	global_position += dir * tile_size
