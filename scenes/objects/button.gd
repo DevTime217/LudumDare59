2dccb5a101c3fd085eb6b603c64e154f8dd71259
DevTime217 <headshotgamingmaster@gmail.com>
2026-04-20 18:17:20 -0400

    Button Code in Progress

--- scenes/objects/button.gd
+++ scenes/objects/button.gd
@@ -1,1 +1,8 @@
 extends StaticBody2D
+
+var button_position = $".".get_transform()
+
+func _ready() -> void:
+	if button_position == get_tree().get_first_node_in_group("Player").get_transform():
+		for node in get_tree().get_nodes_in_group("Door"):
+			node.visible = false
